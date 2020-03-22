a52369827053f7b4b131993e34f8802c4597c3bd
Daniel Krawczyk <dankraw@gmail.com>
2020-03-22 12:53:54 +0100

    v0.3.2

--- Formula/ssh-aliases.rb
+++ Formula/ssh-aliases.rb
@@ -2,8 +2,8 @@ class SshAliases < Formula
   desc "A tool for ~/.ssh/config generation"
   homepage "https://github.com/dankraw/ssh-aliases"
   url "https://github.com/dankraw/ssh-aliases.git",
-      :tag => "v0.3.1",
-    :revision => "54034079d2a1a40000b23ca8d8e2f79136ac852f"
+      :tag => "v0.3.2",
+    :revision => "313365b35cd3f03331b1f791920ae8546c9fe616"
 
   head "https://github.com/dankraw/ssh-aliases.git"
 
